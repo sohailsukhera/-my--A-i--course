b6ad829370f585f94783ced0ebd55bd1a4903fe5
sohailsukhera <sohailsukhera799@gmail.com>
2026-01-30 10:36:59 +0500

    variable6.py

---
+++ variabl6.py
@@ -0,0 +1,202 @@
+# Create a variable age and assign your age to it. Print it.
+# age =input("enter your age:")
+# print("your age is ",age)
+
+
+
+
+#Create two variables a = 10 and b = 20. Print their sum.
+# a = 10 
+# b = 20
+# sum = a+b
+# print (sum)
+
+# name = input ("enter your name:")
+# print ("my name is",name)
+#Store your name in a variable and print:
+
+
+#Create a variable price with value 99.99. Print its type.
+# price = 99.99
+# print (type(price))
+
+
+
+#What will be the output?if x=5, y=x ,print(y)
+# x = 5
+# y = x 
+# print (y)
+
+
+
+
+#Identify the data type of each: 
+# a = 10
+# b = 3.14
+# c = "Python"
+# d = True
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+
+#Write a program that stores:
+# your age (int)
+# your height (float)
+# your name (string)
+# whether you are a student (boolean)
+# Then print all of them with their types.
+
+
+
+# What will be the output?
+# x = "10"
+# y = 5
+
+# x = int("10") 
+# y =  5 
+# sum = x + y 
+# print (sum)
+
+
+
+#Convert an integer num = 25 into a float and print it.
+# num =float ("25")
+# print (num)
+
+
+
+#Convert price = "499" into an integer and print price + 1.
+# price = 499
+# price = (499+1)
+# # print (price)
+
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+# #Which of the following variable names are valid? If invalid, say why.
+#  myname . valid 
+# my_name valid 
+# 2name .invalid
+#  my-name = valid
+#_age  .invalid
+
+
+
+# Create a variable using snake_case for:
+# Total marks = 45
+#  total_marks=45
+
+
+
+
+
+# Rewrite this code using better variable names:
+# x = 500
+# y = 0.18
+# z = x + x*y
+# a = 500
+# b = 0.18 
+# c = a + a*b
+# print (c)
+
+
+
+# Take user input for their name and print:
+# Hello, <name>!
+# name = input ("enter your name :")
+# print("hello",name)
+
+
+
+# Take two numbers from the user and print their sum.
+# a = 30
+# b = 50 
+# sum = a+b
+# print (sum)
+
+
+# Take input for age (as string), convert it to int, and print:
+# Next year your age will be <age +1
+
+
+
+
+# What is the problem in this code?
+# age = input ("Enter your age: ")
+# print (int(age) + 5 )
+
+
+
+# Predict the output:
+# a = 10
+# b = 3
+# print(a / b) 
+# print(a // b)
+# print(a % b)
+
+
+
+# Write a program to calculate the area of a rectangle using:
+# length
+# breadth
+
+
+
+# What will be the output?
+# x = 5
+# x = x + 1
+# x = x * 2
+# print(x)
+
+
+
+
+# What is the type of this variable?
+# value = 10 > 5
+# print (type("value")) 
+
+
+
+# What will happen?
+# a = "Python"
+# b = 3
+# print(a * b) 
+
+
+
+
+# Why does this code give an error?
+# a = 10
+# b =int("20")
+# print (a+b)
+
+# Write a program that:
+# Takes name and age as input
+# Stores them in variables
+# Prints
+# name= input("enter your name:")
+# age = input ("enter your age:")
+# print("my name is" , name)
+# print("my age is" ,age)
+
+
+
+# Take a number as input and print:
+# its square
+# its cube
+# num = int(input("Enter the number:"))
+# print("square:", num * num)
+# print("Cube:", num * num * num)
+# print("Square:", num ** 2)
+# print("Cube:", num ** 3)
+
+
+
+
+
+
+
+
+
+
+
