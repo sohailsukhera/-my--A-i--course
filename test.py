4901e7f580bbc985b899c87953ff0f124067546a
sohailsukhera <sohailsukhera799@gmail.com>
2026-02-15 10:31:53 +0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,152 @@
+# name = input("enter your name:")
+# print ("well come",name)
+
+
+
+
+# name =input("enter your name:")
+# if(len(name))>5:
+#     print ("long name")
+# else:
+#     print("short name")
+
+
+
+
+# name =input("enter your name : ")
+# if name.isupper():
+#     print ("upercase word")
+# elif name.islower():
+#     print("lower case")
+# else:
+#     print ("mix word")
+
+
+
+# str = input("Enter your password :")
+# if len("str") >= 8:
+#     print("accpect password")
+# else:
+#     print ("password short")
+ 
+# email = input("enter your email:")
+# if "@" in email :
+#     print ("vallid email")
+# else:
+#     print("not vallid")
+
+# websit =input("enter your web:")
+# if"ww" in websit:
+#     print ("vallid websit")
+# else:
+#     print("not valid websit")
+# nub = input("enter your number:")
+# if len("nub")>= int(11) :
+#     print("vallid number")
+# else:
+#     print("not vallid number")
+
+# age = int(input("enter your age : "))
+# if age>=int(18) :
+#     print("can vote")
+# else:
+#     print ("can't vote")
+# age = int(input(("enter your age:")))
+# if age >= int(5) :
+#     print("go to school")
+# else:
+#     print("can,t go to school")  
+
+# age = int(input("enter your age:"))
+# if age >= int(18):
+#     print("applly for licens")
+# else:
+#     print ("cant apply for licence")
+# price = int (input("enter your price :"))
+# if price > int(500):
+#     print ("you get a discount")
+# else:
+#     print ("no  discount available")
+
+# amount =int(input("enter your amount:"))
+# if amount >= int(1000):
+#     print("free delivery")
+# else :
+#     print("delivery chagers only")
+
+# price =int(input("enter your price:"))
+# if price < int(100):
+#     print ("cheap product")
+# else:
+#     print("expensive product")
+  
+# hour = int(input("time is :"))
+# if hour > int(5) and hour < int (11):
+#     print ("good morning")
+# if hour > int(12) and hour < int (16):
+#     print ("good after noon")
+# elif hour > int(17) and hour < int (20):
+#     print ("good evening")
+# else:
+#     print("good night")
+
+
+# username = str(input("enter your name : " ))
+# password = int(input("enter your password : "))
+
+# if (username == "user") and (password == 1234 ) : 
+#     print("login successful")
+# else : 
+#     print("login failed")
+# pin = input ("enter your pin:")
+# if len(pin) == 4 :
+#     print ("valid oin")
+# else:
+#     print ("not valid pin")
+  
+ 
+# fruit = input("enter fruit name:")
+# if fruit in("mango(4) apple(5) bnana(7) orange(9)"):
+#     print ("fruit avilable")
+# else:
+#     print("not avialable")
+
+# name = input("enter country name:")
+# if name ==("pakistan"):
+#     print ("well come pakistan")
+# else :
+# print ("invild country")
+
+
+
+# choice = input("yes or no:")
+# if choice == "yes":
+#     print("conformid")
+# else:
+#     print ("cancelled")
+
+
+# char = input("Enter a single character: ")
+
+# if char.isalpha():
+#     print("Alphabet")
+# else:
+#     print("Not an alphabet")
+
+# message = input("enter your messge:")
+# if message == "":
+#     print ("not given input")
+# else:
+#     print("message reviced")
+
+
+# word = input("enter your word:")
+# if word.startswith("py"):
+#     print("python")
+# else :
+#     print("not python")
+
+a = int(input("enter your nbr:"))
+b = int(input("enter your nbr:"))
+
+a,b = b,
