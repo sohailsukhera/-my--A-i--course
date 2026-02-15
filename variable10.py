61a549584dde1d33a014de212cde6fa0188801e3
sohailsukhera <sohailsukhera799@gmail.com>
2026-02-15 10:30:05 +0500

    Create variable10.py

---
+++ variable10.py
@@ -0,0 +1,50 @@
+# name = input("enter your name:")
+# print("wellcome",name)
+# name =( input("enter your name:"))
+# age = (input("enter your age:"))
+# price =(input("enter youe price:"))
+# print("well come", name)
+# print("your age is",age)
+# print("your price is",price)
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+
+# marks = int(input("enter your marks:"))
+# if(marks >=90):
+#     print("grade A")
+# elif(marks >= 80 and marks < 90):
+#     print("grade B")
+# elif(marks >= 70 and marks < 80):
+#     print("grade C")
+# else:
+#     print("grade D")
+# student =["sohail",95.5, 56,"gp"]
+# print(student[0])
+# student[0]="ali"
+# print (student)
+# list = [34,45,67,89,89,45,34,35]
+# (list.append(5))
+# print(list)
+# list =  [3,2,1]
+# (list.sort())
+# print (list)
+# list = ['a','b','c','d']
+# list. reverse()
+# print (list)
+# list = [1,2,3,4,5,6,]
+# list.insert(4,4)
+# list = [1,2,3,4,5,6,7,9]
+# list.pop(3)
+# print (list)
+# tup = (1,2,3,4,5,6,7)
+# print (type(tup))
+# print (tup[0])
+# print(tup[2])
+# tup = (1,2,3,4,5,6,7,8,9,7)
+# print (tup[1:3])
+# tup = (1,2,3,4,5,2,7,2)
+# print (tup.count(2))
+tup = (1,3,2,3,4,5,6,7,)
+print(tup.index())
