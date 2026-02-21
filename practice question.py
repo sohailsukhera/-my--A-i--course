3b588e150a5c7d904a0f14aef0eefd48d91e1e2f
sohailsukhera <sohailsukhera799@gmail.com>
2026-02-21 14:56:25 +0500

    Create practice question.py

---
+++ practice question.py
@@ -0,0 +1,45 @@
+# input and output function 
+# Question (1)
+'''use a program to print "helo world!" and your name on two separets lines .'''
+# print("hello\nworld")
+# question (2)
+'''Ask the user for their favorite colour using input() and print "your favorite is colour(colour)'''
+# colour =input("your favorite colour is: ")
+# question (3)
+#  '''use a singl print () statment to display three diffrent words separated by a hypen(-)'''
+# print("hello_world_well come")
+# question (4)
+'''prompt the user for their birth year and print their age (assume the the current age 2026)'''
+# birthday= int(input("Enter your age:"))
+# age = 2026-birthday
+# print("age",age )
+# question (5)
+'''print the result of 5+5 such that the output is : the sum of  5 and 5 = 10.'''
+# a = 5 
+# b = 5
+# sum = (a+b)
+# print(sum)
+# questoin (6)
+'''use the end parameter in print ( )to join two saparate print statment with a space .'''
+# print("hello \n world")
+# question (7)
+'''rite a program that takes two strings form the users and print them joined togather '''
+# str1 = input("Enter your str:")
+# str2 = input("Enter your str:")
+# str3 = (str1+str2)
+# print(str3)
+# question (8)
+'''creat a greeting that takes a users,s name and print "well come" [name] in all upercase.'''
+# name= input("Enter your name:")
+# print("wellcome :",name.upper())
+# question(9)
+'''ask for user,s city and country , then print them in formate : "city country.'''
+# country = input("what are u country:")
+# city = input("what is your city:")
+# print("country ""city")
+# questiom(10)
+'''experiment : what happen if you try too add a string and a integer in a print statment ?
+write a code snippet that fixes this using str ().'''
+name = ("sohail")
+age = str(23)
+print (name + age)
