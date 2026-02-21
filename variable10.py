41cc36dd8fdd60cfffc37c3596b00ff908246f63
sohailsukhera <sohailsukhera799@gmail.com>
2026-02-21 14:56:48 +0500

    Update variable10.py

--- variable10.py
+++ variable10.py
@@ -46,5 +46,68 @@
 # print (tup[1:3])
 # tup = (1,2,3,4,5,2,7,2)
 # print (tup.count(2))
-tup = (1,3,2,3,4,5,6,7,)
-print(tup.index())
+# tup = (1,3,2,3,4,5,6,7,)
+# print(tup.index())
+
+
+# """1.....bill generator"""
+# # product price 
+# product = float(input("Enter your product price:"))
+# # product quantity
+# quantity = int(input("Enter product quantity:"))
+# subtotal = product+quantity
+# # Total Tax
+# tax = subtotal*.10
+# # Final Total bill
+# Final_Total_Bill= subtotal+tax
+
+# print("\n-------finalbill-------")
+# print("Product Price:", product)
+# print("Quantity of Product:", quantity)
+# print("Tax:", tax)
+# print("Final Bill:", Final_Total_Bill)
+
+
+"""1..... 2nd method 
+                        bill generator"""
+
+
+# product = float(input("Enter your product price:"))
+# quantity = int(input("Enter product quantity:"))
+# total = product*quantity
+# # discount = 0
+# # discount rule
+# if total >  10000 :
+#     discount = total*0.20
+# elif total < 2000:
+#     discount = total*0.10
+# subtotal = total - discount 
+# tax = subtotal * 0.10
+# totalbill = subtotal+tax
+# print("\n-------final bill-------")
+# print("total :", total)
+# print("discount:", discount)
+# print("amount after discount:", subtotal)
+# print("tax:", tax)
+# print("Final Payable Bill:", totalbill)
+
+# print("Welcome to Shope Billing System")
+price = float(input("Enter product price: "))
+quantity = int(input("Enter quantity: "))
+total = price*quantity
+# discount rule
+if total > 10000: 
+    discount = total*0.20
+elif total < 2000:
+    discount = total*0.10
+
+subtotal = total - discount
+# tax 12%
+tax = subtotal * 0.12
+final_bill = subtotal + tax
+print("\n-------BILL RECEIPT-------")
+print("Total amount:", total)
+print("Discount:", discount)
+print("Amount after discount:", subtotal)
+print("Tax:", tax)
+print("Final Payable Bill:", final_bill)
