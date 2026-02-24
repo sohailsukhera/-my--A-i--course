23a534111f952a6b7e28d1c10d5bdeb70ea609f3
sohailsukhera <sohailsukhera799@gmail.com>
2026-02-24 16:38:19 +0500

    Create practice question 3.py

---
+++ practice question 3.py
@@ -0,0 +1,67 @@
+# Ametic oprators 
+# # question(21)rith
+'''write a program to calculate the aera of a rtaecngle (length*width)'''
+# length = float(input("Enter your length:"))
+# width = float(input("Enter your width:"))
+# aera = length*width
+# print("final length",aera)
+# question (22)
+'''take two numbers and print the result  ofthe first raised to the power of the secend (a^b)'''
+# a =  int(input("Enter your number:"))
+# b = int (input("Enter your number :"))
+# result =(a**b)
+# print ("final reuslt",result)
+# question (23)
+'''Demonstrate the diffrence between / (divison)and //(floor divison) with the number 10 and 3 '''
+# a = 10
+# b = 3 
+# result1 = a / b
+# result2 = a//b
+# print(result1)
+#  print(result2)
+# question (24)
+'''use the modulus opraters % to find the reminder when 25 is divided by 4'''
+# a = 25 
+# b = 4 
+# result = 25 % 4 
+# print(result)
+# question (25)
+'''calculate the average of 5 numbers entered by the users '''
+# a = float(input("Enter your number:"))
+# b= float(input("Enter your number:"))
+# c = float(input("Enter your number:"))
+# d = float(input("Enter your number:"))
+# f = float(input("Enter your number:"))
+# average = (a+b+c+d+f)/5
+# print("average",average)
+# question (26)
+'''create a program that converts minutes  into hours and remaing minutes'''
+# minutes = int(input("Enter total minutes:"))
+# hours = minutes//60
+# remaing_minutes = minutes % 60
+# print ("hours:",hours)
+# print ("remaing_minutes:",remaing_minutes)
+# question (27)
+'''caluclate the area of a circal whera eara = \pir(use.3.14 for \pi)'''
+# radius = float(input("Enter your radius:"))
+# pi = 3.14
+# area = pi *(radius**2)
+# print("circal ka area ha :",area)
+# question (28)
+'''find the cube of number enter by the users '''
+# a = float(input("ente your number:"))
+# cube = a * 3
+# print("cube:",cube)
+# question (29)
+'''perform the colaction 10+5*2.dose python follow PEMADS? pove into python code '''
+# a = 10 + 5 * 2 
+# print("result of the 10 + 5*2:",a) 
+# question (30)
+'''write a program to caluculations simple interest :(p\)times r\times t)\100'''
+# principal = float(input("Principal amount (P) enter karein: "))
+# rate = float(input("Rate of interest (R in %) enter karein: "))
+# time = float(input("Time (T in years) enter karein: "))
+
+# simple_interest = (principal * rate * time) / 100
+
+# print("Simple Interest hai:", simple_interest)
