3dab6a0ae1895735dbcb315e91fb419af28ca967
sohailsukhera <sohailsukhera799@gmail.com>
2026-02-26 21:48:18 +0500

    Create practice question 4 .py

---
+++ practice question 4 .py
@@ -0,0 +1,76 @@
+# comparison logical opretors 
+# question (31)
+'''compare  two numbers entered by users and print if the first is greater then secend'''
+# first  = input ("Enter your number:")
+# secend = input ("Enter your number :")
+# if first > secend:
+#     print ("grater then secend")
+# else :
+#     print("eroor")
+# question (32)
+'''check if a user entered number is even (number%2==0) and print the boolean result .'''
+# number = float(input("Enter your number:"))
+# if number%2 == 0 :
+#     print("even number")
+# else: 
+#     print("boolen")
+# question (33)
+'''write a program thats check if a number is between 10 and 50 (inclusive)using and '''
+# number = int(input("Enter your number:"))
+# if number >= 10 and number <=50:
+#     print("true")
+# else: 
+#     print("false")
+# question (34)
+'''check if a string enterd by the user is equl to "python"'''
+
+# str = (input("Enter your word :"))
+# if str == "python":
+#     print("valied string")
+# else :
+#     print("not valied string")
+# question (35)
+'''use the or opreator to check if a user is either "admin"or "superuser"'''
+# name = input("Enter your name:")
+# name2 = input("Enter your name:")
+# if name1 == "admin" or name2 == "superuser" :
+#     print("name valied")
+# else:
+#     print("not valied")
+# question (36)
+'''demonstrate the not oprator by reversing a boolen variable'''
+# str = ("true")
+# print("orignal value:", "true")
+# print("resvered value:",not "true")
+# question (37)
+'''compare two flooting_point numbers 0.1 +0.2 ==0.3 explain the result'''
+# a = 0.1
+# b = 0.2
+# sum = a == b
+# print(sum)
+# question (38)
+
+'''take user age and check if they are not under 18'''
+# age =int (input("enter your age:"))
+# if age > 18:
+#     print ("valied age") 
+# else:
+#     print(" not valied")
+# question (39)
+'''ck if a number is postive and ood using logical opreaters '''
+# number = int(input("enter your number:"))
+# if number == 0 and number % 2 != 0 :
+#     print("odd number")
+# else:
+#     print ("not odd")
+# question (40)
+'''comparea the length of two strings provide by the users'''
+# str1 =float(input("Enter your length :"))
+# str2 =float(input("Enter your length:"))
+# if str1 > str2:
+#     print("str1 is big")
+# elif str1 < str2:
+#     print("str2 is big ")
+# else: 
+#     print("str1 is equal str2")
+
