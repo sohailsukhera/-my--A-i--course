ed1755e448275bbf01a45264cb132efe0e50c49b
sohailsukhera <sohailsukhera799@gmail.com>
2026-03-07 10:30:14 +0500

    Create practice question 5.py

---
+++ practice question 5.py
@@ -0,0 +1,49 @@
+# assignment operotors with Arithmetic 
+# qiuestion (41)
+'''initialize a variable x = 10.use += to add 5  to it ''' 
+# x = 10 
+# x += 5
+# print(x)
+# question (42)
+'''use -= to subtract 3 form a  variable price '''
+# x= 10 
+# x -=3 
+# print(x)
+# question (43)
+'''multiplye a variable balance by 2 using the *= opreators'''
+# a = 20 
+# a *=2
+# print(a)
+# question (44)
+'''divide the variable  total by 4  using the /= opreator '''
+# price = 40
+# price /= 4
+# print(price)
+# question (45)
+'''Use **= suquare the variable '''
+# b = 3 
+# b **= 3 
+# print(b)
+# question (46)
+'''Create a counter  variable and increment it by 1 using  assignment opreators'''
+# a = 24 
+# a += 1
+# print(a)
+# question (47)
+'''use %= to find the reminder of a variable divied by 2 abs update the variable'''
+# price = 10
+# price /= 2
+# print(price) 
+# question (48)
+'''use //= tp perform floor divison on a variable and update it '''
+# a = 45 
+# a //= 5
+# print(a)
+# question (49)
+'''start with n = 2 three line of  code using   assignment opreators trun it into 20'''
+# a = 2
+# a += 8
+# a *= 20/10
+# print(a)
+
+
