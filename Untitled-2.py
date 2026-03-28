f5b4e69e9e12600cc837622f88fa051ca98cd07d
sohailsukhera <sohailsukhera799@gmail.com>
2026-03-28 10:17:05 +0500

    Create Untitled-2.py

---
+++ Untitled-2.py
@@ -0,0 +1,84 @@
+# print("hello world ")
+# name1 = input("enter youe name :")
+# name2 = input("enter your name :")
+# print("well come",name1)
+# print("well come",name2)
+# name =input ("enteryour name:")
+# marks = input("enter your marks:")
+# age   = input("enter your age :")
+# print ("your name",name)
+# print ("your marks",marks)
+# print ("your age",age)
+
+# a = 10 
+# b = 40 
+# sum = a+b
+# print (sum)
+# light = "red"
+# if light == "green":
+#     print("go")
+# elif light == "yellow":
+#     print("look")
+# else :
+#     print("light is broken") 
+# a = 4.5
+# b = int("5")
+# sum = (a+b) 
+# print(sum)    
+
+# a = float(input("enter your nbr :"))
+# print(type(a))
+
+
+# marks =float(input("enter your marks :"))
+# if marks > 95 :
+#     print("grade A")
+# elif marks > 70  and marks < 90:
+#     print ("grade B")
+# else : 
+#     print ("grade C")
+
+# str1 = ("ali is a good boy\n")
+# str2 = ("ali is a bad boy")
+# str = (str1 + str2)
+# print(str)
+# print(len(str))
+
+# str1 = ("ali is a very good boy ")
+# print(str1[:])
+
+# str = ("i am a good boy")
+# print(str.endswith("oy"))
+# str1 = ("ali is a good boy")
+# print(str1.capitalize())
+# str2 = ("sohail is a very good boy")
+# print(str2.count("o"))
+# str3 = ("ahmad is a very good boy ")
+# print(str3.find("is"))
+# str4 = ("we well play game in the match of pakistan")
+# print(str4.center("str4"))
+# str5 = ("Ahmad brought a new car form the show room of ali khan")
+# print(str5.replace ("o","a"))
+# marks = float(input("Enter your marks :"))
+# if marks > 90 :
+#     print("a")
+# elif marks  >= 80 and marks < 90 :
+#     print("B")
+# else:
+#     print("c")
+
+# list= [45.,45,67,90,45,23,12,]
+# print(list [1:4])
+# list = [1,2,3,4]
+# list.append(9)
+# print(list)
+
+
+# list = [2,3,2,5,2,7,8,9]
+# list.pop(6)
+# print(list)
+
+a = (1,2,3,3,3,6)
+print (a.count(2))
+
+
